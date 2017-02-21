6beea18350acea52e2ac0a1a659a8067fb3ff278
cbogues <YOUR-EMAIL-ADDRESS>
2017-02-21 14:22:43 -0800

    quantity slider and final commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 	const priceInput = document.querySelector('[name=price]');
 	const quantityInput = document.querySelector('[name=quantity]');
 	const total = document.querySelector('.total');
-
+	const quantityLabel = document.querySelector('.quantity-label');
 
 	// create the functions that we need
 
@@ -21,9 +21,17 @@
 
 	}
 
+	function updateQuantityLabel () {
+		const quantity = quantityInput.value;
+		quantityLabel.innerText = quantity;
 
+	} 
+
+	// on first run
+	calculatePieCost();
 
 
 	// add event listeners
 	priceInput.addEventListener('input', calculatePieCost);
 	quantityInput.addEventListener('input', calculatePieCost);
+	quantityInput.addEventListener('input', updateQuantityLabel);
