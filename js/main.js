17ec2eb9024faf1d81671be1405783541598e05b
cbogues <YOUR-EMAIL-ADDRESS>
2017-02-21 13:54:42 -0800

    moved css file and have quantity done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-(function() {
 
-	
+	// grab everything we need from DOM
+	const priceInput = document.querySelector('[name=price]');
+	const quantityInput = document.querySelector('[name=quantity]');
+	const total = document.querySelector('.total');
 
-});
+
+	// create the functions that we need
+
+	function calculatePieCost() {
+		console.log(priceInput.value);
+		const price = priceInput.value;
+		const quantity = quantityInput.value;
+
+
+		const cost = price * quantity;
+
+		console.log(cost);
+
+		total.innerText = '$' + cost.toFixed(2);
+
+	}
+
+
+
+
+	// add event listeners
+	priceInput.addEventListener('input', calculatePieCost);
+	quantityInput.addEventListener('input', calculatePieCost);
